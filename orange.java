2f0e990f47d60a47544d6cfd8c64d456ff82306c
kozimster <36714161+kozimster@users.noreply.github.com>
2019-03-10 22:22:31 +0000

    Add files via upload

---
+++ orange.java
@@ -0,0 +1,29 @@
+
+/* PP 2.2 Write an application that reads three integers and prints their
+average.*/
+
+import java.util.Scanner;
+public class orange {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int num1, num2, num3, average;
+		
+		System.out.println("Enter first number:");
+		num1 = scan.nextInt();
+		System.out.println("Enter second number:");
+		num2 = scan.nextInt();
+		System.out.println("Enter third number:");
+		num3 = scan.nextInt();
+		
+		average = (num1 + num2 + num3)/3;
+		
+		System.out.println("Your numbers are: " + num1 + " " + num2 + " " + num3);
+		System.out.println("Average is: "  + average);
+		
+
+	}
+
+}
